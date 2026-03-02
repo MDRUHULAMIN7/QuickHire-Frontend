4ce363772b4ab38de7d1bb63ad4ede8e97dd3d62
mdruhulamin7 <ruhulthisis@gmail.com>
2026-03-02 22:56:07 +0600

    update Hero Section Degine

--- src/components/hero/Hero.tsx
+++ src/components/hero/Hero.tsx
@@ -22,7 +22,7 @@ export default function Hero() {
               Discover
               <br /> more than
               <br />
-              <span className="text-[#26A4FF]">5000+ Jobs</span>
+              <span className="text-[#26A4FF] ">5000+ Jobs</span>
             </h1>
             <div className="-mt-1 mb-6">
               <Image
